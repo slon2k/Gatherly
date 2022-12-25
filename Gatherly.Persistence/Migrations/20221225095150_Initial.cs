5a3bd7b1ea225e8a7563b517e495eaf68774fc50
Boris Korobeinikov <Boris_Korobeinikov@epam.com>
2022-12-25 15:52:04 +0600

    Initial migration

---
+++ Gatherly.Persistence/Migrations/20221225095150_Initial.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gatherly.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Member",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Member", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gathering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ScheduledDate = table.Column<DateTime>(type: "Date", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    MaxNumberOfAttendees = table.Column<int>(type: "int", nullable: true),
+                    InvitationsExpireAt = table.Column<DateTime>(type: "Date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gathering", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Gathering_Member_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "Member",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attendee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MemberId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GatheringId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "Date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attendee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attendee_Gathering_GatheringId",
+                        column: x => x.GatheringId,
+                        principalTable: "Gathering",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Attendee_Member_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Member",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invitation",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MemberId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GatheringId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "Date", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "Date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invitation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invitation_Gathering_GatheringId",
+                        column: x => x.GatheringId,
+                        principalTable: "Gathering",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Invitation_Member_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Member",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendee_GatheringId",
+                table: "Attendee",
+                column: "GatheringId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendee_MemberId_GatheringId",
+                table: "Attendee",
+                columns: new[] { "MemberId", "GatheringId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gathering_CreatorId",
+                table: "Gathering",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invitation_GatheringId",
+                table: "Invitation",
+                column: "GatheringId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invitation_MemberId",
+                table: "Invitation",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Member_Email",
+                table: "Member",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attendee");
+
+            migrationBuilder.DropTable(
+                name: "Invitation");
+
+            migrationBuilder.DropTable(
+                name: "Gathering");
+
+            migrationBuilder.DropTable(
+                name: "Member");
+        }
+    }
+}
